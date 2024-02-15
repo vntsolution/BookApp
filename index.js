547bec07a337ca43aa872f1b0593167fd71549ae
vntsolution <159558408+vntsolution@users.noreply.github.com>
2024-02-15 22:26:07 +0530

    Update index.js
    abc

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const authRoutes = require("./routes/add_routes");
 const conn = require("./db/connection.js");
 console.log("Get All ma aaviyu ")
 
+app.get("/",(req,res)=>{
+     const All = await AddModel.find().then((data) => { res.send(data) })
+})
 app.use("/Books", authRoutes)
 // app.use("/",conn)
 app.listen(port, () => {

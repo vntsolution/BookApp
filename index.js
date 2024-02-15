9c93322b06c3f91192ad91719765b35343c4c76d
vntsolution <159558408+vntsolution@users.noreply.github.com>
2024-02-15 22:04:33 +0530

    Update index.js
    10.04

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const port = 3000;
 require("./db/connection")
 app.use(express.json())
 const authRoutes = require("./routes/add_routes");
+const conn = require("./db/connection.js");
 console.log("Get All ma aaviyu ")
 
 app.use("/Books", authRoutes)
-
+app.use("/",conn)
 app.listen(port, () => {
     console.log("Server start at port no :" + port)
 })

5bdb7a27ad9812c53c17fc62a2dd436a747fa4fd
Vinit <ss.vinitdevani@gmail.com>
2024-02-16 11:45:12 +0530

    Change in controller

--- controller/addcontroller.js
+++ controller/addcontroller.js
@@ -21,7 +21,7 @@ const getById = async (req, res) => {
         const All = await AddModel.findOne({ "_id": id }).then((data) => { res.send({ data: data }) })
         // res.status(200).json({data: AllIds[0], success: true })
     } catch (error) {
-        res.status(500).json({ message: error, success: false })
+        res.status(500).json({ message: error.message, success: false })
     }
 }
 
@@ -58,19 +58,31 @@ const updateId = async (req, res) => {
         console.log("Upate ma aaviyu");
 
         const id = req.body.id;
-        await AddModel.updateOne({ _id: id }, {
-            $set: {
-                department_name: body.department_name, department_thumbh: body.department_thumbh,
-                corse_name: body.corse_name, corse_thumbh: body.corse_thumbh, semister: body.semister,
-                subject_name: body.subject_name, subject_thumbh: body.subject_thumbh,
-                subject_discription: body.subject_discription, authar: body.authar, book_name: body.book_name,
-                book_link: body.book_link, book_thumbh: body.book_thumbh, book_discription: body.book_discription,
-                book_rating: body.book_rating
+        await AddModel.findOneAndUpdate({ _id: id },
+            {
+                $set: {
+                    department_name: req.body.department_name,
+                    department_thumbh: req.body.department_thumbh,
+                    corse_name: req.body.corse_name,
+                    corse_thumbh: req.body.corse_thumbh,
+                    semister: req.body.semister,
+                    subject_name: req.body.subject_name,
+                    subject_thumbh: req.body.subject_thumbh,
+                    subject_discription: req.body.subject_discription,
+                    authar: req.body.authar,
+                    book_name: req.body.book_name,
+                    book_link: req.body.book_link,
+                    book_thumbh: req.body.book_thumbh,
+                    book_discription: req.body.book_discription,
+                    book_rating: req.body.book_rating
+                }
             }
-        }).then(async (data) => {
+        ).then(async (data) => {
             console.log("hello");
-            await AddModel.find({}).then((data) => { res.send(data) })
-        })
+            await AddModel.findOne({ "_id": id }).then((data) => {
+                res.status(200).json({ message: "Data Update Sucessfully", success: true, data: data });
+            });
+        });
 
         // res.status(200).json({ data: "user success", success: true })
     } catch (error) {
